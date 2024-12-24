53546a3988be8a213065a6a94a73b2eceffd1853
Zak Lawrence <zlawrence97@outlook.com>
2024-12-24 12:13:09 +0000

    Solution to day 24 part 1 and start on part 2

---
+++ Day24/day24.py
@@ -0,0 +1,188 @@
+import sys
+sys.path.insert(0,"/Users/zlawrence/Documents/LocalWorkArea/Advent-Of-Code-2024")
+from shared.common import read_file
+from shared.GraphTools import Graph
+from graphviz import Digraph, Graph as GVGraph
+from collections import defaultdict
+
+function = {
+    "AND": (lambda x1,x2: x1 & x2),
+    "OR": (lambda x1,x2: x1 | x2),
+    "XOR": (lambda x1,x2: x1 ^ x2),
+}
+
+class LogicGate:
+    def __init__(self, name:str):
+        self.name = name
+        self.inputs = []
+
+    def add_input(self,gate):
+        self.inputs.append(gate)
+
+    def remove_input(self,gate_name):
+        self.inputs = [gate for gate in self.inputs if gate.name != gate_name]
+
+    def output(self):
+        pass
+
+class InputGate(LogicGate):
+    def __init__(self, name, value=False):
+        super().__init__(name)
+        self.value = value
+    
+    def set_value(self,value):
+        self.value = value
+    
+    def output(self):
+        return self.value
+
+class ANDGate(LogicGate):
+    def output(self):
+        return all(input_gate.output() for input_gate in self.inputs)
+
+class ORGate(LogicGate):
+    def output(self):
+        return any(input_gate.output() for input_gate in self.inputs)
+
+class XORGate(LogicGate):
+    def output(self):
+        return sum(input_gate.output() for input_gate in self.inputs) == 1
+
+class LogicNetwork(Graph):
+    def __init__(self,inputs,gates):
+        def get_gates(gates):
+            connections = []
+            logic_gates = {}
+            for key, gate in gates.items():
+                connections.append((gate["in1"],key,1))
+                connections.append((gate["in2"],key,1))
+                if gate['function'] == "AND":
+                    logic_gates[key] = ANDGate(key) 
+                if gate['function'] == "OR":
+                    logic_gates[key] = ORGate(key) 
+                if gate['function'] == "XOR":
+                    logic_gates[key] = XORGate(key) 
+            return connections,logic_gates
+
+        self.input_layer = {key:InputGate(key,value) for key,value in inputs.items()}
+        self.connections, self.gates = get_gates(gates)
+        self.gates = self.gates | self.input_layer
+        for in1,gate,_ in self.connections:
+            self.gates[gate].add_input(self.gates[in1])
+        super().__init__(self.connections,True)
+
+    def output(self,input_x, input_y):
+        inputs = binary_rep(input_x,"x") | binary_rep(input_y,"y")
+        for key,value in inputs.items():
+            self.gates[key].set_value(value)
+        out = {}
+        for i in range(46):
+            out[f"z{i:02}"] = self.gates[f"z{i:02}"].output()
+        return out
+    
+    def visualize(self, output_file="graph", format="png"):
+        """Visualize the graph using Graphviz."""
+        # Choose the appropriate Graphviz class
+        gv_graph = Digraph() if self._directed else GVGraph()
+
+        # Add nodes and edges
+        for node1, neighbors in self._graph.items():
+            gv_graph.node(node1)  # Add the node
+            for node2, weight in neighbors.items():
+                # Add edges with weights
+                gv_graph.edge(node1, node2, label=str(weight))
+
+        # Render the graph
+        gv_graph.render(output_file, format=format, cleanup=True)
+        print(f"Graph saved to {output_file}.{format}")
+    
+def binary_rep(num,key):
+    bin_rep = bin(num)[2:].zfill(45)
+    bin_dict = {}
+    for i,bit in enumerate(reversed(bin_rep)):
+        bin_dict[f"{key}{i:02}"] = int(bit)
+    return bin_dict
+
+def build_output(results):
+    outputs = {k:v for k,v in results.items() if k.startswith("z")}
+    total = 0 
+    for k,v in outputs.items():
+        if v:
+            total += 2**int(k[1:])
+    return total
+
+def build_system(gates,inputs):
+    while None in inputs.values():
+        for gk,gate in gates.items():
+            in1 = inputs[gate["in1"]]
+            in2 = inputs[gate["in2"]]
+            if in1 != None and in2 != None:
+                inputs[gk] = function[gate["function"]](in1,in2)
+    return inputs
+
+def parse_input(path):
+    inputs = {}
+    values = {}
+    gates = {}
+
+    def parse_wire(line):
+        line = line.split(":")
+        inputs[line[0]] = int(line[1].strip()) == 1
+        values[line[0]] = int(line[1].strip()) == 1
+    
+    def pasrse_gate(line):
+        line = line.split("->")
+        key = line[1].strip()
+        gate = line[0].strip()
+        in1,func,in2 = gate.split(" ")
+        gates[key] = {"function": func,"in1":in1, "in2":in2}
+        values[key] = None
+
+    file = read_file(path)
+    for line in file:
+        line = line.strip()
+        if line == "":
+            continue
+        if ":" in line:
+            parse_wire(line)
+        else:
+            pasrse_gate(line)
+    return values,inputs,gates
+
+def part1(path):
+    values,_,gates = parse_input(path)
+    results = build_system(gates,values)
+    output = build_output(results)
+    print(f"Part 1: {output}")
+
+def compare_dics(dict1,dict2):
+    differences = []
+    for key in dict1:
+        if dict1[key] != dict2[key]:
+            differences.append((key,dict1[key],dict2[key]))
+    return differences
+
+def part2(path):
+    values,inputs,gates = parse_input(path)
+    network = LogicNetwork(inputs,gates)
+
+    one = 1
+    for i in range(45):
+        x = one <<i
+        y = one <<i
+        z = x+y 
+        sum_z = binary_rep(z,"z")
+        result = network.output(x,y)
+        print(i,compare_dics(sum_z,result))
+
+
+
+    network.visualize()
+
+def main(day:int,Test:bool=False):
+    path = f"Day{day}/test.txt" if Test else f"Day{day}/input.txt" 
+    part1(path)
+    part2(path)
+
+if __name__ == "__main__":
+    main(24)
---
+++ shared/GraphTools.py
@@ -0,0 +1,115 @@
+from collections import defaultdict
+import heapq
+from itertools import combinations
+
+class Graph(object):
+    def __init__(self,connections, directed = False):
+        self._graph = defaultdict(dict)
+        self._directed = directed
+        self.add_connections(connections)
+
+    def add_connections(self,connections):
+        for node1, node2, weight in connections:
+            self.add(node1,node2,weight)
+
+    def add(self,node1,node2,weight):
+        self._graph[node1][node2] = weight
+        if not self._directed:
+            self._graph[node2][node1] = weight
+    
+    def remove(self,node):
+        for n,cxns in self._graph.items():
+            cxns.pop(node,None)
+        self._graph.pop(node,None)
+
+    def remove_connection(self, node1, node2):
+        if node1 in self._graph and node2 in self._graph[node1]:
+            del self._graph[node1][node2]  
+            if not self._directed and node2 in self._graph and node1 in self._graph[node2]:
+                del self._graph[node2][node1] 
+        else:
+            raise ValueError(f"No connection exists between {node1} and {node2}")
+
+    def is_connected(self,node1,node2):
+        return node1 in self._graph and node2 in self._graph[node1]
+    
+    def get_weight(self,node1,node2):
+        return self._graph[node1].get(node2,None)
+    
+    def local_graph(self,node1):
+        if node1 in self._graph:
+            return self._graph[node1]
+        else:
+            raise ValueError(f"Cannot find node {node1} in graph!")
+    
+    def update_weight(self,node1,node2,weight):
+        if node1 in self._graph and node2 in self._graph[node1]:
+            self._graph[node1][node2] = weight
+            if not self._directed:
+                self._graph[node2][node1] = weight
+        else: 
+            raise ValueError(f"No edge between {node1} and {node2}")
+
+    def find_path_length(self,start,end):
+        if start not in self._graph or end not in self._graph:
+            return False,float('inf')
+
+        queue = [(0,start)]
+        visited = set()
+        distance = {start:0}
+
+        while queue:
+            current_distance, current_node = heapq.heappop(queue)
+            if current_node in visited:
+                continue
+            visited.add(current_node)
+
+            if current_node == end:
+                return True,current_distance
+            
+            for neighbour, weight in self._graph[current_node].items():
+                if neighbour not in visited:
+                    new_distance = current_distance+weight
+                    if new_distance < distance.get(neighbour,float('inf')):
+                        distance[neighbour] = new_distance
+                        heapq.heappush(queue,(new_distance,neighbour))
+        return False, float('inf')
+
+    def has_path(self, start, end):
+        exists, _ = self.find_path_length(start, end)
+        return exists
+
+    def find_cliques(self,n):
+        if n <=1:
+            raise ValueError(f"Must search for groups of size greater than 1!")
+        nodes = list(self._graph.keys())
+        cliques = []
+        for subset in combinations(nodes,n):
+            if self.is_clique(subset):
+                cliques.append(set(subset))
+        return cliques
+    
+    def is_clique(self,subset):
+        for i,node1 in enumerate(subset):
+            for node2 in subset[i+1:]:
+                if node2 not in self._graph[node1]:
+                    return False
+        return True
+    
+    def find_largest_clique(self):
+        def Bron_Kerbosch(R,P,X):
+            if not P and not X:
+                maximal_cliques.append(R)
+                return
+            for v in list(P):
+                Bron_Kerbosch(R|{v},P & self._graph[v].keys(), X & self._graph[v].keys())
+                P.remove(v)
+                X.add(v)
+        maximal_cliques = []
+        Bron_Kerbosch(set(),set(self._graph.keys()),set())
+        largest_clique = max(maximal_cliques,key=len)
+        return largest_clique
+
+    def __str__(self):
+        """Pretty-print the graph structure."""
+        return str(dict(self._graph))
